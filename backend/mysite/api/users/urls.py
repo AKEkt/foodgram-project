0f922a2602683483541e2a7fb475d66c03c2243b
Антон Колесников <A.K.Ekt@yandex.ru>
2023-04-17 23:03:52 +0500

    edit_permiss

--- backend/mysite/api/users/urls.py
+++ backend/mysite/api/users/urls.py
@@ -10,13 +10,12 @@ router_v1 = DefaultRouter()
 router_v1.register(r'users', CustomUsersViewSet, basename='users')
 
 urlpatterns = [
-
+    path('', include(router_v1.urls)),
     path('users/me/', CustomUsersViewSet.as_view({'get': 'me'})),
     # path('users/<int:pk>/', CustomUsersViewSet.as_view({'get': 'retrieve'})),
     path('users/subscriptions/', ListSubscripViewSet.as_view({'get': 'list'})),
     path('users/<int:pk>/subscribe/', FollowViewSet.as_view()),
     path('auth/token/login/', CustomTokenCreateView.as_view()),
     path('auth/token/logout/', TokenDestroyView.as_view()),
-    path('', include(router_v1.urls)),
 
 ]
