3b6a178790172f1bfe926f7a5c2a2b78d84a6690
Антон Колесников <A.K.Ekt@yandex.ru>
2023-04-20 18:34:47 +0500

    admin

--- backend/mysite/recipes/urls.py
+++ backend/mysite/recipes/urls.py
@@ -1,17 +1,14 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from .views import (DownloadShopCart, FavoriteViewSet, RecipesViewSet,
                     ShoppingCartViewSet)
 
-# from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
 
-
-# router = DefaultRouter()
-
-# router.register(r'', RecipesViewSet, basename='recipes')
+router.register(r'', RecipesViewSet, basename='recipes')
 urlpatterns = [
-    # path('', include(router.urls)),
-    path('', RecipesViewSet.as_view()),
+    path('', include(router.urls)),
     path('download_shopping_cart/', DownloadShopCart.as_view()),
     path('<int:pk>/favorite/', FavoriteViewSet.as_view()),
     path('<int:pk>/shopping_cart/', ShoppingCartViewSet.as_view()),
