d9a3ee284f022095cf4968f0c720862fec491919
Anton Kolesnikov <A.K.Ekt@yandex.ru>
2023-04-24 13:22:45 +0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-
+    'PAGE_SIZE': 1000,
 }
 DJOSER = {
     'SERIALIZERS': {
